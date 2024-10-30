f25928aa35a79e99dba90a7d3d9a3089bcfb4a2c
MaxAfreezmaker <adamskimaksymilian1@gmail.com>
2024-10-30 18:42:29 +0100

    Zadanie 3 Linkowanie wszystkiego w NavBar #3

--- LabMax/src/components/NavBarMenu.jsx
+++ LabMax/src/components/NavBarMenu.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import RootLayout, {menuItems} from '../layout/RootLayout';
 
 function NavBarMenu({ items }) {
-return (
+  return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Frameworki frontendowe</Navbar.Brand>
+        <Navbar.Brand href="/">Frameworki frontendowe</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {items.map((item)=>(
-            <Nav.Link key={item.id} href={`${item.label.toLowerCase().replace(/\s+/g, '-')}`}>
-            {item.label}
-            </Nav.Link>
+            {items.map((item) => (
+              <Nav.Link key={item.id} href={item.urlPattern}>
+                {item.label}
+              </Nav.Link>
             ))}
           </Nav>
         </Navbar.Collapse>
