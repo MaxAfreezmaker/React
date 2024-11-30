84b0117534e72e2e05c4a828abf0b086467fdfa2
Maksymilian Adamski <maksymilian.adamski@microsoft.wsei.edu.pl>
2024-11-30 14:15:56 +0100

    Nowy projekt do oddania

---
+++ labmaxnowy/app/(protected)/layout.js
@@ -0,0 +1,12 @@
+import SideBar from "@/components/SideBar";
+
+function Protected ({children}){
+    return(
+        <>
+        {children}
+        
+        </>
+    );
+}
+
+export default Protected;
---
+++ labmaxnowy/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>   
+      <button className="bg-lime-400 p-2 rounded-lg hover:bg-red-600">Click</button> 
+      <button className="btn btn-primary">DaisyUi</button>
+    </>
+  );
+}
